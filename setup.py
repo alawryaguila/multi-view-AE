be8da7013b59ece48bdf41c271597a801423850d
alawryaguila <ana.lawryaguila@outlook.com>
2021-02-02 17:45:15 +0000

    Add updated setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ import os
 with open('multiview_models/requirements.txt') as f:
     required = f.read().splitlines()
 setup(
-    name="multiview_models",
-    version="1.0.0",   
-    packages=find_packages(),
+    name='multiview_models',
+    version='1.0.0',   
+    packages=find_packages(exclude=['*tests']),
     author='Ana Lawry Aguila',
-    url="TODO",
+    author_email='ana.aguila.18@ucl.ac.uk',
+    url='https://github.com/alawryaguila/multiview_models',
     install_requires=[required],
-    description="A library for running multiview models on medical imaging data"
+    description='A library for running multiview models on medical imaging data'
 )
