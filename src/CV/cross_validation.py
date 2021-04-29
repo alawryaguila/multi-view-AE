61d4f6fb47e3c0c44c6a17a7e78e88af764e242b
alawryaguila <ana.lawryaguila@outlook.com>
2021-04-29 14:36:31 +0100

    Update cross validation code

--- src/CV/cross_validation.py
+++ src/CV/cross_validation.py
@@ -11,6 +11,7 @@ from os.path import join
 import datetime
 import json
 from sklearn.model_selection import KFold
+from joblib import Parallel, delayed
 class CrossValidation:
     def __init__(self, config, parameters, model_type=None):
         self.config = config
@@ -18,31 +19,43 @@ class CrossValidation:
         self.model_type = model_type
         self.output_path = self.config['output_dir']
 
-    def run_CV(self, *data, k_folds=2):
+    def gridsearch(self, *data, k_folds=5, jobs=0):
 
         kfold = KFold(n_splits=k_folds, shuffle=True,random_state=42) #check default split size
         param_combs = list(it.product(*(self.parameters[key] for key in self.parameters)))
-        input_dims = [np.shape(data_)[1] for data_ in data]
+        self.input_dims = [np.shape(data_)[1] for data_ in data]
         param_labels_dict = {}
-        cv_dict = {}
         for i, param_comb in enumerate(param_combs):        
             param_labels_dict['Comb {0}'.format(i)] = {key: param_comb[idx] for idx, key in enumerate(self.parameters)}
-            cv_dict['Comb {0}'.format(i)] = {}
-            print("parameters: ", param_labels_dict['Comb {0}'.format(i)])
-            for fold, (train_ids, val_ids) in enumerate(kfold.split(data[0])):
-                print("fold: ", fold)
-                train_data, val_data = self.select_data(train_ids, val_ids, *data)
-                model = self.init_model(input_dims, param_labels_dict['Comb {0}'.format(i)])
-                model.fit(*train_data)
-                same_recon, cross_recon = model.MSE_results(*val_data)
-                cv_dict['Comb {0}'.format(i)]['fold {0}'.format(fold)] = {'same view MSE': round(same_recon, 4), 'cross view MSE': round(cross_recon, 4)}
-        #save validation results
+        
+        if jobs>0:
+            print("running array job")
+            cv_results = Parallel(n_jobs=jobs)(delayed(self.kfold_cv)(kfold,  param_labels_dict['Comb {0}'.format(i)], *data) for i, param_comb in enumerate(param_combs))
+        else:
+            cv_results = [self.kfold_cv(kfold, param_labels_dict['Comb {0}'.format(i)], *data) for i, param_comb in enumerate(param_combs)]
+        cv_dict = {}
+        for i in range(len(param_combs)):        
+            cv_dict['Comb {0}'.format(i)] = cv_results[i]
+            
+        if self.config['sparse']:
+            self.model_type = 'sparse_' + self.model_type  
         outpath = self.format_folder()
         json.dump(param_labels_dict,open(join(outpath, 'parameter_labels.json'),'w'))
         json.dump(cv_dict,open(join(outpath, 'cv_results.json'),'w'))
         self.param_labels_dict = param_labels_dict 
         self.cv_dict = cv_dict
 
+    def kfold_cv(self, kfold, parameters, *data):
+        cv_dict = {}
+        for fold, (train_ids, val_ids) in enumerate(kfold.split(data[0])):
+            print("fold: ", fold)
+            train_data, val_data = self.select_data(train_ids, val_ids, *data)
+            model = self.init_model(self.input_dims, parameters)
+            model.fit(*train_data)
+            same_recon, cross_recon = model.MSE_results(*val_data)
+            cv_dict['fold {0}'.format(fold)] = {'same view MSE': round(same_recon, 4), 'cross view MSE': round(cross_recon, 4)}
+        return cv_dict
+        
     def init_model(self, input_dims, parameters):
         self.input_dims = input_dims
         DEVICE = torch.device("cuda")
@@ -60,7 +73,6 @@ class CrossValidation:
         else:
             print("model type not recognised")
             exit()
-
     def select_data(self, train_ids,val_ids, *data):
         train_data = []
         val_data = []
