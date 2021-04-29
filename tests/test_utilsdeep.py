14e0cef9bcb388048452d5b9a171adb8d5afa257
alawryaguila <ana.lawryaguila@outlook.com>
2021-04-29 17:04:32 +0100

    add tests config file and update tests

--- tests/test_utilsdeep.py
+++ tests/test_utilsdeep.py
@@ -1,8 +1,14 @@
-from models import utils_deep
-
+from src.models import utils_deep
+from src.utils.io_utils import ConfigReader
+import numpy as np
 def lets_check_this_works():
     pass
 
-def test_VAE_input():
-    from models.vae import VAE
-    
+def test_VAE():
+    from src.models.vae import VAE
+    DEVICE = 'cpu'
+    config_file = ConfigReader('./test_config.yaml')
+    view_1 = np.random.rand(200, 20)
+    view_2 = np.random.rand(200, 20)
+    models = VAE(input_dims=[20, 20], config=config_file._conf).to(DEVICE)
+    models.fit()
