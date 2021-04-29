4932828509b57ac9d392f1fce9e0b1bb12b9f307
alawryaguila <ana.lawryaguila@outlook.com>
2021-04-29 17:58:14 +0100

    update utils init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -6,8 +6,6 @@ from .kl_utils import *
 __all__ = [
     'MyDataset',
 ]
-
-__all__.extend(datasets.__all__)
 __all__.extend(calc_utils.__all__)
 __all__.extend(io_utils.__all__)
 __all__.extend(kl_utils.__all__)
