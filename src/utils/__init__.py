d03efb0d24deea1f4beb0b684a205eec1635f0a9
alawryaguila <ana.lawryaguila@outlook.com>
2021-04-29 17:55:58 +0100

    update utils init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,11 @@
-from .datasets import *
+from .datasets import MyDataset
 from .calc_utils import *
 from .io_utils import *
 from .kl_utils import *
 
-__all__ = []
+__all__ = [
+    'MyDataset',
+]
 
 __all__.extend(datasets.__all__)
 __all__.extend(calc_utils.__all__)
