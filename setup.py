373f3976d8620816c8a01e83b9719df180b4fcf9
alawryaguila <ana.lawryaguila@outlook.com>
2021-04-29 15:46:19 +0100

    update setup and requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 import os
 
-src_dir = os.path.join(os.getcwd(), 'src')
-packages = {"" : "src"}
-for package in find_packages("src"):
-    packages[package] = "src"
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='multiview_models',
     version='1.0.0',   
-    packages=packages.keys(),
+    packages=find_packages(exclude=['*tests']),
     package_dir={"":"src"},
     author='Ana Lawry Aguila',
     author_email='ana.lawryaguila@outlook.com',
     url='https://github.com/alawryaguila/multiview_models',
+    install_requires=[required],
     description='A library for running multiview models on medical imaging data'
 )
