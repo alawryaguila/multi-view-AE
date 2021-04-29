a0b1430e83f09c03668adb1eb6e5911334ea274c
alawryaguila <ana.lawryaguila@outlook.com>
2021-04-29 18:03:12 +0100

    add init

--- src/CV/__init__.py
+++ src/CV/__init__.py
@@ -1,6 +1,2 @@
 from .cross_validation import *
 
-__all__ = []
-
-
-__all__.extend(cross_validation.__all__)
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,11 +4,3 @@ from .ae import *
 from .utils_deep import *
 from .layers import *
 
-__all__ = []
-
-
-__all__.extend(vae.__all__)
-__all__.extend(joint_vae.__all__)
-__all__.extend(ae.__all__)
-__all__.extend(utils_deep.__all__)
-__all__.extend(layers.__all__)
