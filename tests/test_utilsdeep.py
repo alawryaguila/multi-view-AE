44232d476119d918713c39aaeaf89927cfae4ae7
alawryaguila <ana.lawryaguila@outlook.com>
2021-04-30 10:22:29 +0100

    update test file

--- tests/test_utilsdeep.py
+++ tests/test_utilsdeep.py
@@ -7,7 +7,7 @@ def lets_check_this_works():
 def test_VAE():
     from src.models.vae import VAE
     DEVICE = 'cpu'
-    config_file = ConfigReader('./test_config.yaml')
+    config_file = ConfigReader('./tests/test_config.yaml')
     view_1 = np.random.rand(200, 20)
     view_2 = np.random.rand(200, 20)
     models = VAE(input_dims=[20, 20], config=config_file._conf).to(DEVICE)
