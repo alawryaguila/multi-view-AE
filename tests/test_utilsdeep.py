5830cef69a47c4f6e3c746b3ea252496435a115c
alawryaguila <ana.lawryaguila@outlook.com>
2021-04-30 12:04:05 +0100

    fixing class error

--- tests/test_utilsdeep.py
+++ tests/test_utilsdeep.py
@@ -2,11 +2,10 @@ from src.models import utils_deep
 from src.utils.io_utils import ConfigReader
 import numpy as np
 import itertools as it
-def lets_check_this_works():
-    pass
+
 
 class test_deepmodels:
-    def test_setup():
+    def test_setup(self):
         self.train_1 = np.random.rand(200, 20)
         self.train_2 = np.random.rand(200, 20)
         self.test_1 = np.random.rand(50, 20)
@@ -14,7 +13,7 @@ class test_deepmodels:
         self.config_file = ConfigReader('./tests/test_config.yaml')
         self.DEVICE = 'cpu'
         self.parameters = {'sparse': [True, False], 'batch_size': [None, 10]}
-    def test_VAE():
+    def test_VAE(self):
         from src.models.vae import VAE
         param_combs = list(it.product(*(self.parameters[key] for key in self.parameters)))
         config = self.config_file._conf
@@ -25,7 +24,7 @@ class test_deepmodels:
             models.fit(self.train_1, self.train_2)
             latent_1, latent_2 = models.predict_latents(self.test_1, self.test_2)
             recon = models.predict_reconstruction(self.test_1, self.test_2)
-    def test_jointVAE():
+    def test_jointVAE(self):
         from src.models.joint_vae import VAE
         param_combs = list(it.product(*(self.parameters[key] for key in self.parameters)))
         config = self.config_file._conf
