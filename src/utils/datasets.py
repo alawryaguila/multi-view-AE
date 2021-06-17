e9091266081362589155e920972cf5f1e1241957
alawryaguila <ana.lawryaguila@outlook.com>
2021-06-17 12:04:26 +0100

    Add datasets.py required for classifier VAE

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -23,7 +23,7 @@ class MyDataset(Dataset):
         self.data = data
         if isinstance(data,list):
             if isinstance(data,np.ndarray):
-                self.data = [torch.from_numpy(d).float() for d in self.data ]
+                self.data = [torch.from_numpy(d).float() for d in self.data]
             self.N = len(self.data[0])
             self.shape = np.shape(self.data[0])
         else:
@@ -52,6 +52,40 @@ class MyDataset(Dataset):
     def __len__(self):
         return self.N
 
+class MyDataset_labels(Dataset):
+    def __init__(self, data, labels, indices = False, transform=None):
+        self.data = data
+        self.labels = labels
+        if isinstance(data,list):
+            if isinstance(data,np.ndarray):
+                self.data = [torch.from_numpy(d).float() for d in self.data]
+                
+            self.N = len(self.data[0])
+            self.shape = np.shape(self.data[0])
+        else:
+            if isinstance(data,np.ndarray):
+                self.data = torch.from_numpy(self.data).float()
+            self.N = len(self.data)
+            self.shape = np.shape(self.data)
+        self.labels = torch.from_numpy(self.labels).float()
+        self.transform = transform
+        self.indices = indices
+
+    def __getitem__(self, index):
+        if isinstance(self.data,list):
+            x = [d[index] for d in self.data]
+        else:
+            x = self.data[index]
+
+        if self.transform:
+            x = self.transform(x)
+        t = self.labels[index]
+        if self.indices:
+            return x, t, index
+        return x, t
+
+    def __len__(self):
+        return self.N
 
 class GeneratorUniform(torch.nn.Module):
     
