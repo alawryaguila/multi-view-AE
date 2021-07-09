fbe400b22b25d68f3f1c529da97b0a807c36c650
alawryaguila <ana.lawryaguila@outlook.com>
2021-07-09 17:56:16 +0100

    Add AAE and AAE with wasserstein loss models

---
+++ src/models/aae.py
@@ -0,0 +1,143 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal
+from .layers import Encoder, Decoder, Discriminator 
+from .utils_deep import Optimisation_AAE
+import numpy as np
+from torch.autograd import Variable 
+class AAE(nn.Module, Optimisation_AAE):
+    
+    def __init__(self, input_dims, config):
+
+        '''
+        
+        Initialise Adversarial Autoencoder model.
+
+        input_dims: The input data dimension.
+        config: Configuration dictionary.
+
+        
+        '''
+
+        super().__init__()
+        self._config = config
+        self.model_type = 'AAE'
+        self.input_dims = input_dims
+        self.hidden_layer_dims = config['hidden_layers'].copy()
+        self.z_dim = config['latent_size']
+        self.hidden_layer_dims.append(self.z_dim)
+        self.non_linear = config['non_linear']
+        self.SNP_model = config['SNP_model']
+        self.learning_rate = config['learning_rate']
+        self.n_views = len(input_dims)
+        self.encoders = torch.nn.ModuleList([Encoder(input_dim = input_dim, hidden_layer_dims=self.hidden_layer_dims, variational=False, non_linear=self.non_linear) for input_dim in self.input_dims])
+        self.decoders = torch.nn.ModuleList([Decoder(input_dim = input_dim, hidden_layer_dims=self.hidden_layer_dims, variational=False, non_linear=self.non_linear) for input_dim in self.input_dims])
+        self.discriminator = Discriminator(input_dim = self.z_dim, hidden_layer_dims=[3], output_dim=(self.n_views+1))
+        
+        self.encoder_optimizers = [torch.optim.Adam(list(self.encoders[i].parameters()), lr=self.learning_rate) for i in range(self.n_views)]
+        self.generator_optimizers = [torch.optim.Adam(list(self.encoders[i].parameters()), lr=self.learning_rate) for i in range(self.n_views)]
+        self.decoder_optimizers = [torch.optim.Adam(list(self.decoders[i].parameters()), lr=self.learning_rate) for i in range(self.n_views)]
+        self.discriminator_optimizer = torch.optim.Adam(list(self.discriminator.parameters()), lr=self.learning_rate)
+    
+    def encode(self, x):
+        z = []
+        for i in range(self.n_views):
+            z_ = self.encoders[i](x[i])
+            z.append(z_)
+
+        return z
+    
+
+    def decode(self, z):
+        x_same = []
+        x_cross = []
+        for i in range(self.n_views):
+            for j in range(self.n_views):
+                x_out = self.decoders[i](z[j])
+                if i == j:
+                    x_same.append(x_out)
+                else:
+                    x_cross.append(x_out)
+        return x_same, x_cross
+    
+    def disc(self, z):
+        z_real = Variable(torch.randn(z[0].size()[0], self.z_dim) * 1.).to(self.device)
+        d_real = self.discriminator(z_real)
+        d_fake = []
+        for i in range(self.n_views):
+            d = self.discriminator(z[i])
+            d_fake.append(d)
+        return d_real, d_fake
+
+    def forward_recon(self, x):
+        z = self.encode(x)
+        x_same, x_cross = self.decode(z)
+        fwd_rtn = {'x_same': x_same,
+                    'x_cross': x_cross,
+                    'z': z}
+        return fwd_rtn
+    
+    def forward_discrim(self, x):
+        [encoder.eval() for encoder in self.encoders]
+        z = self.encode(x)
+        d_real, d_fake = self.disc(z)
+        fwd_rtn = {'d_real': d_real,
+                    'd_fake': d_fake,
+                    'z': z}
+        return fwd_rtn
+
+    
+    def forward_gen(self, x):
+        [encoder.train() for encoder in self.encoders]
+        self.discriminator.eval()
+        z = self.encode(x)
+        _, d_fake = self.disc(z)
+        fwd_rtn = {'d_fake': d_fake,
+                    'z': z}
+        return fwd_rtn
+
+    @staticmethod
+    def recon_loss(self, x, fwd_rtn):
+        x_cross = fwd_rtn['x_cross']
+        x_same = fwd_rtn['x_same']
+        recon_loss = 0
+        for i in range(self.n_views):
+            recon_loss+= torch.mean(((x_same[i] - x[i])**2).sum(dim=-1))
+            recon_loss+= torch.mean(((x_cross[i] - x[i])**2).sum(dim=-1))
+        return recon_loss/self.n_views/self.n_views
+
+    @staticmethod
+    def generator_loss(self, fwd_rtn):
+        z = fwd_rtn['z']
+        d_fake = fwd_rtn['d_fake']
+        gen_loss = 0
+        label_real = np.zeros((z[0].shape[0], self.n_views+1))
+        label_real[:,0] = 1
+        label_real = torch.FloatTensor(label_real).to(self.device)
+        for i in range(self.n_views):
+            gen_loss+= -torch.mean(label_real*torch.log(d_fake[i]+self.eps))
+        return gen_loss/self.n_views
+
+    @staticmethod
+    def discriminator_loss(self, fwd_rtn):
+        z = fwd_rtn['z']
+        d_real = fwd_rtn['d_real']
+        d_fake = fwd_rtn['d_fake']
+        disc_loss = 0
+        label_real = np.zeros((z[0].shape[0], self.n_views+1))
+        label_real[:,0] = 1
+        label_real = torch.FloatTensor(label_real).to(self.device)
+        
+        disc_loss+= -torch.mean(label_real*torch.log(d_real+self.eps))
+        for i in range(self.n_views):
+            label_fake = np.zeros((z[0].shape[0], self.n_views+1))
+            label_fake[:,i+1] = 1
+            label_fake = torch.FloatTensor(label_fake).to(self.device)
+            disc_loss+= -torch.mean(label_fake*torch.log(d_fake[i]+self.eps))
+
+        return disc_loss/(self.n_views+1)
+
+__all__ = [
+    'AAE'
+]
--- src/models/utils_deep.py
+++ src/models/utils_deep.py
@@ -6,6 +6,7 @@ Wrapper classes for VAE, AE and DVCCA models
 from ..utils.datasets import MyDataset, MyDataset_SNPs, MyDataset_labels
 import numpy as np
 import torch
+from torchvision import transforms
 from ..utils.io_utils import Logger
 from ..plot.plotting import Plotting
 import datetime
@@ -19,23 +20,28 @@ class Optimisation_VAE(Plotting):
 
     def generate_data(self, data, labels=None):
         batch_sz = self._config['batch_size']
-        if self.SNP_model:  
-            data = MyDataset_SNPs(data)
-            generator = torch.utils.data.DataLoader(
-                data,
-                batch_size=batch_sz,
-                shuffle=False,
-                **self.kwargs_generator
-            )
-            return [generator]
+        if self.SNP_model: 
+            generators = [] 
+            for data_ in data:
+                data = MyDataset_SNPs(data)
+                generator = torch.utils.data.DataLoader(
+                    data,
+                    batch_size=batch_sz,
+                    shuffle=False,
+                    **self.kwargs_generator
+                )
+                generators.append(generator)
+
+            return generators
         elif labels is not None:
             generators = []
             for data_ in data:
-                if self._config['batch_size']!=None:
+                if self._config['batch_size'] is not None:
                     batch_sz = self._config['batch_size']
                 else:
                     batch_sz = np.shape(data_)[0]
                 data_ = MyDataset_labels(data_, labels)
+                
                 generator = torch.utils.data.DataLoader(
                     data_,
                     batch_size=batch_sz,
@@ -43,6 +49,7 @@ class Optimisation_VAE(Plotting):
                     **self.kwargs_generator
                 )
                 generators.append(generator)
+
             return generators
         else: 
             generators = []
@@ -52,6 +59,7 @@ class Optimisation_VAE(Plotting):
                     batch_sz_tmp = self._config['batch_size']
                 else:
                     batch_sz_tmp = np.shape(data_)[0]
+    
                 data_ = MyDataset(data_)
                 generator = torch.utils.data.DataLoader(
                     data_,
@@ -61,11 +69,13 @@ class Optimisation_VAE(Plotting):
                 )
                 generators.append(generator)
                 del generator
+        
             return generators
 
 
     def preprocess(self, generators, labels=None):
-        if labels is not None:             
+        if labels is not None:   
+            #print(generators)      
             for batch_idx, batch in enumerate(zip(*generators)):
                 data = [data_[0].to(self.device) for data_ in batch]
                 labels = [data_[1].to(self.device, dtype=torch.int64) for data_ in batch]
@@ -75,7 +85,6 @@ class Optimisation_VAE(Plotting):
             for batch_idx, data in enumerate(zip(*generators)):
                 data = [data_.to(self.device) for data_ in data]
             return data
-
 
     def init_optimisation(self):
         self.train()
@@ -87,10 +96,19 @@ class Optimisation_VAE(Plotting):
         self.init_optimisation()
         self.to(self.device)
         self.epochs = self._config['n_epochs']
-
+        self.transform = transform=transforms.Normalize([0], [1])
         for epoch in range(1, self.epochs + 1):
             if self.batch_size is not None and generators is not None:
                 for batch_idx, (local_batch) in enumerate(zip(*generators)):
+                    #print(local_batch[0])
+                    if self.transform:
+                        local_batch = [self.transform(local_batch_.unsqueeze(0)).squeeze(0) for local_batch_ in local_batch] #not working atm need to fix   
+                        for local_batch_ in local_batch:
+                            local_batch_[torch.isnan(local_batch_)] = 0   
+                    #print(local_batch[0])
+                    #exit()  
+                    #x = torch.nan_to_num(x)
+               
                     if self.labels is not None:
                         labels = [local_batch_[1].to(self.device, dtype=torch.int64) for local_batch_ in local_batch]
                         local_batch = [local_batch_[0].to(self.device) for local_batch_ in local_batch] 
@@ -114,7 +132,6 @@ class Optimisation_VAE(Plotting):
                 logger.on_train_init(log_keys)
             else:
                 logger.on_step_fi(loss)
-        
         return logger
 
     def optimise_batch(self, local_batch):
@@ -162,6 +179,7 @@ class Optimisation_VAE(Plotting):
         use_cuda = use_GPU and torch.cuda.is_available()
         self.kwargs_generator = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
         self.device = torch.device("cuda" if use_cuda else "cpu")
+        self.eps = 1e-15 
 
         generators = self.generate_data(self.data, labels=self.labels)
         if self.batch_size is not None:
@@ -172,6 +190,7 @@ class Optimisation_VAE(Plotting):
             else:
                 data = self.preprocess(generators)
             logger = self.optimise(data=data)
+        self.end_optimisation()
         if self._config['save_model']:
             model_path = os.path.join(self.output_path, 'model.pkl')
             while os.path.exists(model_path):
@@ -183,7 +202,6 @@ class Optimisation_VAE(Plotting):
                     model_path = os.path.join(self.output_path, option + '.pkl')
             torch.save(self, model_path)
             self.plot_losses(logger)
-        self.end_optimisation()
             
         return logger
 
@@ -204,22 +222,33 @@ class Optimisation_VAE(Plotting):
             os.makedirs(output_path)   
         return output_path
 
-    def predict_latents(self, *data):
-        generators =  self.generate_data(data)
-        if self.batch_size==None:
-            data = self.preprocess(generators)
+    def predict_latents(self, *data, labels=None):
+        self.data = data
+        self.labels = labels
+        generators =  self.generate_data(self.data, labels=self.labels)
+        if self.batch_size is None:
+            if self.labels is not None:
+                data = self.preprocess(generators, labels=True)
+            else:
+                data = self.preprocess(generators)
         num_elements = len(generators[0].dataset)
         batch_size = generators[0].batch_size
 
         num_batches = len(generators[0])
         with torch.no_grad():
-            if self.batch_size!=None:
+            if self.batch_size is not None:
                 if self.joint_representation:
                     predictions = torch.zeros(num_elements, int(self._config['latent_size']))
                 else:
                     predictions = [torch.zeros(num_elements, int(self._config['latent_size'])) for i in range(len(generators))]
                 for batch_idx, local_batch in enumerate(zip(*generators)):
-                    local_batch = [local_batch_.to(self.device) for local_batch_ in local_batch]
+                    if self.labels is not None:
+                        labels = [local_batch_[1].to(self.device, dtype=torch.int64) for local_batch_ in local_batch]
+                        local_batch = [local_batch_[0].to(self.device) for local_batch_ in local_batch] 
+                        labels = labels[0]
+                        local_batch = [local_batch, labels]
+                    else:
+                        local_batch = [local_batch_.to(self.device) for local_batch_ in local_batch]
                     mu, logvar = self.encode(local_batch)
                     pred = self.reparameterise(mu, logvar)
                     if self.sparse:
@@ -294,6 +323,130 @@ class Optimisation_VAE(Plotting):
                         x_recon_temp = [(self.sample_from_normal(x_recon_)).cpu().detach().numpy() for x_recon_ in x_recon[i]]
                         x_recon_out.append(x_recon_temp)
             return x_recon_out
+
+class Optimisation_AAE(Optimisation_VAE):
+    
+    def __init__(self):
+        super().__init__()
+
+    def optimise_batch(self, local_batch):
+        self.init_optimisation()
+        fwd_return = self.forward_recon(local_batch)
+        loss_recon = self.recon_loss(self, local_batch, fwd_return)
+        [optimizer.zero_grad() for optimizer in self.encoder_optimizers]
+        [optimizer.zero_grad() for optimizer in self.decoder_optimizers]
+        loss_recon.backward()
+        [optimizer.step() for optimizer in self.encoder_optimizers]
+        [optimizer.step() for optimizer in self.decoder_optimizers]
+
+        fwd_return = self.forward_discrim(local_batch)
+        loss_disc = self.discriminator_loss(self, fwd_return)
+        self.discriminator_optimizer.zero_grad() 
+        loss_disc.backward()
+        self.discriminator_optimizer.step() 
+        if self.wasserstein:
+            for p in self.discriminator.parameters():
+                p.data.clamp_(-0.01, 0.01)
+        fwd_return = self.forward_gen(local_batch)
+        loss_gen = self.generator_loss(self, fwd_return) 
+        [optimizer.zero_grad() for optimizer in self.generator_optimizers]
+        loss_gen.backward()
+        [optimizer.step() for optimizer in self.generator_optimizers]
+
+        loss = {'recon': loss_recon,
+                'disc': loss_disc,
+                'gen': loss_gen}
+        return loss
+
+    def predict_reconstruction(self, *data):
+        generators =  self.generate_data(data)
+        if self.batch_size==None:
+            data = self.preprocess(generators)
+        num_elements = len(generators[0].dataset)
+        batch_size = generators[0].batch_size
+        num_batches = len(generators[0])
+        with torch.no_grad():
+            if self.batch_size!=None:
+                x_same = []
+                x_cross = []
+                for batch_idx, local_batch in enumerate(zip(*generators)):
+                    local_batch = [local_batch_.to(self.device) for local_batch_ in local_batch]
+                    z = self.encode(local_batch)
+                    if self.joint_representation:
+                        x_same_ = self.decode(z)
+                    else:
+                        x_same_, x_cross_ = self.decode(z)
+                    start = batch_idx * batch_size
+                    end = start + batch_size
+                    if batch_idx == num_batches - 1:
+                        end = num_elements
+                    for i in range(len(generators)):
+                        if self.joint_representation:
+                            x_same[i][start:end, :] = x_same_[i]
+                        else:
+                            x_same[i][start:end, :] = x_same_[i]
+                            x_cross[i][start:end, :] = x_cross_[i]
+            else:
+                z = self.encode(data)
+                if self.joint_representation:
+                    x_same = self.decode(z)
+                else:
+                    x_same, x_cross = self.decode(z)
+        if self.joint_representation:
+            return [x_same_.cpu().detach().numpy() for x_same_ in x_same]
+        return [x_same_.cpu().detach().numpy() for x_same_ in x_same], [x_cross_.cpu().detach().numpy() for x_cross_ in x_cross]
+
+    def MSE_results(self, *data): 
+        cross_recon = 0
+        same_recon = 0
+        with torch.no_grad():
+            if self.joint_representation:
+                x_same = self.predict_reconstruction(*data)
+            else:
+                x_same, x_cross = self.predict_reconstruction(*data)
+            if self.joint_representation:  
+                for i in range(self.n_views):
+                    #same view prediction
+                    same_temp = np.mean((x_same[i] - data[i])**2)
+                    same_recon+= same_temp
+                    data_input = list(data)
+                    data_input[i] = np.zeros(np.shape(data[i]))
+                    #cross view prediction
+                    pred_cross = self.predict_reconstruction(*data_input)
+                    cross_temp = np.mean((pred_cross[i] - data[i])**2)
+                    cross_recon+= cross_temp  
+            else:
+                for i in range(self.n_views):
+                    same_temp = np.mean((x_same[i] - data[i])**2)
+                    same_recon+= same_temp
+                    cross_temp = np.mean((x_cross[i] - data[i])**2)
+                    cross_recon+= cross_temp
+            return same_recon/self.n_views, cross_recon/self.n_views 
+
+    def predict_latents(self, *data):
+        generators =  self.generate_data(data)
+        if self.batch_size==None:
+            data = self.preprocess(generators)
+        num_elements = len(generators[0].dataset)
+        batch_size = generators[0].batch_size
+        num_batches = len(generators[0])
+        with torch.no_grad():
+            if self.batch_size!=None:
+                predictions = [torch.zeros(num_elements, int(self._config['latent_size'])) for i in range(len(generators))]
+                for batch_idx, local_batch in enumerate(zip(*generators)):
+                    local_batch = [local_batch_.to(self.device) for local_batch_ in local_batch]
+                    pred = self.encode(local_batch)
+                    start = batch_idx * batch_size
+                    end = start + batch_size
+                    if batch_idx == num_batches - 1:
+                        end = num_elements
+                    for i, pred_ in enumerate(pred):
+                        predictions[i][start:end, :] = pred_
+            else:
+                predictions = self.encode(data)
+
+        return [predictions_.cpu().detach().numpy() for predictions_ in predictions]
+
 
 class Optimisation_DVCCA(Optimisation_VAE):
     
