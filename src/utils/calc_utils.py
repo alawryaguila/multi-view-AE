5c764888ed34cbdbb04caf0476789e375e40ef76
alawryaguila <ana.lawryaguila@outlook.com>
2021-08-01 11:17:50 +0100

    Tidy up calc_utils.py file

--- src/utils/calc_utils.py
+++ src/utils/calc_utils.py
@@ -6,16 +6,25 @@ MeanRepresentation: class to create mean representation from separate representa
 import numpy as np
 import torch
 import torch.nn as nn
+from scipy import stats
+
 def calc_corr(x1, x2, corr_type='pearson'):
+    #TODO - write test
     if corr_type=='pearson':
         return np.corrcoef(x1,x2)
+    if corr_type=='kendalltau':
+        return stats.kendalltau(x1, x2)  
+    if corr_type=='spearman':
+        return stats.spearmanr(x1, x2)
+
 
 class ProductOfExperts(nn.Module):
-    """Return parameters for product of independent experts.
+    '''Return parameters for product of independent experts.
     See https://arxiv.org/pdf/1410.7827.pdf for equations.
-    @param mu: M x D for M experts
-    @param logvar: M x D for M experts
-    """
+
+    :param mu: M x D for M experts
+    :param logvar: M x D for M experts
+    '''
     def forward(self, mu, logvar, eps=1e-8):
         var       = torch.exp(logvar) + eps
         # precision of i-th Gaussian expert at point x
@@ -26,10 +35,10 @@ class ProductOfExperts(nn.Module):
         return pd_mu, pd_logvar
 
 class MeanRepresentation(nn.Module):
-    """Return mean of separate VAE representations.
-    @param mu: M x D for M views
-    @param logvar: M x D for M views
-    """
+    '''Return mean of separate VAE representations.
+    :param mu: M x D for M views
+    :param logvar: M x D for M views
+    '''
     def forward(self, mu, logvar):
         mean_mu     = torch.mean(mu, axis=0)
         mean_logvar = torch.mean(logvar, axis=0)
