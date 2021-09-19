b177aa5eb74169e4da6c30e4abdee8ad2833c9d3
alawryaguila <ana.lawryaguila@outlook.com>
2021-09-19 18:43:25 +0100

    fix AAE reconstruction prediction

--- src/models/utils_deep.py
+++ src/models/utils_deep.py
@@ -7,7 +7,7 @@ from ..plot.plotting import Plotting
 import datetime
 import os
 from sklearn.model_selection import KFold
-
+import random 
 class Optimisation_VAE(Plotting):
     
     def __init__(self):
@@ -40,6 +40,7 @@ class Optimisation_VAE(Plotting):
                 generator = torch.utils.data.DataLoader(
                     data_,
                     batch_size=batch_sz,
+              
                     shuffle=False,
                     **self.kwargs_generator
                 )
@@ -66,9 +67,32 @@ class Optimisation_VAE(Plotting):
                 del generator
         
             return generators
+     
+    def data_split(self, data, split=0.9):
+        random.seed(42)
+        idx_1 = list(random.sample(range(0, data[0].shape[0]), int(data[0].shape[0]*0.8)))
+        idx_2 = np.setdiff1d(list(range(data[0].shape[0])),idx_1)
+        data_1 = []
+        data_2 = []
+        for data_ in data:
+            data_1_ = data_[idx_1,:]
+            data_2_ = data_[idx_2,:]
+            data_1.append(data_1_)
+            data_2.append(data_2_)
 
+        return [data_1, data_2]
 
     def preprocess(self, generators, labels=None):
+        if self.val_set:
+            data = []
+            for generators_ in generators:
+                data_ = self.preprocess_run(generators_, labels)
+                data.append(data_)
+        else:
+            data = self.preprocess_run(generators, labels)
+        return data
+
+    def preprocess_run(self, generators, labels=None):
         if labels is not None:   
             for batch_idx, batch in enumerate(zip(*generators)):
                 data = [data_[0].to(self.device) for data_ in batch]
@@ -79,7 +103,6 @@ class Optimisation_VAE(Plotting):
             for batch_idx, data in enumerate(zip(*generators)):
                 data = [data_.to(self.device) for data_ in data]
             return data
-
     def init_optimisation(self):
         self.train()
     
@@ -95,26 +118,18 @@ class Optimisation_VAE(Plotting):
         return data
 
     def optimise(self, generators=None, data=[], verbose=True):
-        self.init_optimisation()
         self.to(self.device)
         self.transform = transform=transforms.Normalize(0, 1)
-        for epoch in range(1, self.n_epochs + 1):
-            if self.batch_size is not None and generators is not None:
-                for batch_idx, (local_batch) in enumerate(zip(*generators)):
-                    if self.SNP_model:
-                        local_batch = [self.centre_SNPs(local_batch_, self.MAF_file) for local_batch_ in local_batch]  
-                    if self.labels is not None:
-                        labels = [local_batch_[1].to(self.device, dtype=torch.int64) for local_batch_ in local_batch]
-                        local_batch = [local_batch_[0].to(self.device) for local_batch_ in local_batch] 
-                        labels = labels[0]
-                        local_batch = [local_batch, labels]
-                    else:
-                        local_batch = [local_batch_.to(self.device) for local_batch_ in local_batch]
+        if self.val_set:
+            if generators is not None:
+                generators, val_generators = generators[0], generators[1]
+            else:
+                data, val_data = data[0], data[1]
 
-                    loss = self.optimise_batch(local_batch)
-                    if batch_idx  == 0 and verbose:
-                        to_print = 'Train Epoch: ' + str(epoch) + ' ' + 'Train batch: ' + str(batch_idx) + ' '+ ', '.join([k + ': ' + str(round(v.item(), 3)) for k, v in loss.items()])
-                        print(to_print)
+        for epoch in range(1, self.n_epochs + 1):
+            self.init_optimisation()
+            if self.batch_size is not None and generators is not None:
+                loss = self.iterate_batch(generators, epoch, verbose=True)
             else:
                 loss = self.optimise_batch(data)
                 if verbose:
@@ -126,7 +141,39 @@ class Optimisation_VAE(Plotting):
                 logger.on_train_init(log_keys)
             else:
                 logger.on_step_fi(loss)
+
+            if self.val_set:
+                if self.batch_size is not None and val_generators is not None:
+                    val_loss = self.iterate_batch(val_generators, epoch, verbose=True)
+                else:
+                    val_loss = self.validate_batch(val_data)
+                    to_print = 'val Epoch: ' + str(epoch) + ' ' + ', '.join([k + ': ' + str(round(v.item(), 3)) for k, v in val_loss.items()])
+                    print(to_print)
+                if epoch == 1:
+                    log_keys = list(val_loss.keys())
+                    self.val_logger = Logger()
+                    self.val_logger.on_train_init(log_keys)
+                else:
+                    self.val_logger.on_step_fi(val_loss)
         return logger
+
+    def iterate_batch(self, generators, epoch, verbose):
+        for batch_idx, (local_batch) in enumerate(zip(*generators)):
+            if self.SNP_model:
+                local_batch = [self.centre_SNPs(local_batch_, self.MAF_file) for local_batch_ in local_batch]  
+            if self.labels is not None:
+                labels = [local_batch_[1].to(self.device, dtype=torch.int64) for local_batch_ in local_batch] #check this - is int64 right?
+                local_batch = [local_batch_[0].to(self.device) for local_batch_ in local_batch] 
+                labels = labels[0]
+                local_batch = [local_batch, labels]
+            else:
+                local_batch = [local_batch_.to(self.device) for local_batch_ in local_batch]
+
+            loss = self.validate_batch(local_batch) if self.val_set else self.optimise_batch(local_batch)
+            if batch_idx  == 0 and verbose:
+                to_print = 'Train Epoch: ' + str(epoch) + ' ' + 'Train batch: ' + str(batch_idx) + ' '+ ', '.join([k + ': ' + str(round(v.item(), 3)) for k, v in loss.items()])
+                print(to_print)
+        return loss
 
     def optimise_batch(self, local_batch):
         fwd_return = self.forward(local_batch)
@@ -134,6 +181,13 @@ class Optimisation_VAE(Plotting):
         [optimizer.zero_grad() for optimizer in self.optimizers]
         loss['total'].backward()
         [optimizer.step() for optimizer in self.optimizers]
+        return loss
+
+    def validate_batch(self, local_batch):
+        with torch.no_grad():
+            self.eval()
+            fwd_rtn = self.forward(local_batch)
+            loss = self.loss_function(local_batch, fwd_rtn)
         return loss
 
     def MSE_results(self, *data): 
@@ -163,7 +217,7 @@ class Optimisation_VAE(Plotting):
                             cross_recon+= cross_temp
             return same_recon/self.n_views, cross_recon/self.n_views        
 
-    def fit(self, *data, labels=None, MAF_file=None, use_GPU=True, save_model=False, n_epochs=200, batch_size=None, **kwargs):
+    def fit(self, *data, labels=None, MAF_file=None, use_GPU=True, save_model=True, n_epochs=2000, batch_size=None, val_set=True, **kwargs):
         self.use_GPU = use_GPU
         self.save_model = save_model
         self.n_epochs = n_epochs
@@ -177,8 +231,17 @@ class Optimisation_VAE(Plotting):
         self.kwargs_generator = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
         self.device = torch.device("cuda" if use_cuda else "cpu")
         self.eps = 1e-15 
+        self.val_set = val_set
+        self.__dict__.update(kwargs)
 
-        generators = self.generate_data(self.data, labels=self.labels)
+        if self.val_set:
+            data = self.data_split(data)
+            generators = []
+            for data_ in data:
+                generators_ = self.generate_data(data_, labels=self.labels)
+                generators.append(generators_)
+        else:
+            generators = self.generate_data(data, labels=self.labels)
         if self.batch_size is not None:
             logger = self.optimise(generators=generators)
         else:
@@ -199,7 +262,8 @@ class Optimisation_VAE(Plotting):
                     model_path = os.path.join(self.output_path, option + '.pkl')
             torch.save(self, model_path)
             self.plot_losses(logger)
-            
+            if self.val_set:
+                self.plot_losses(self.val_logger, title='_validation')
         return logger
 
     def specify_folder(self, path=None):
@@ -210,7 +274,7 @@ class Optimisation_VAE(Plotting):
         return self.output_path
 
     def format_folder(self, output_dir='./'):
-        init_path = self.output_dir
+        init_path = output_dir
         model_type = self.model_type
         date = str(datetime.date.today())
         date = date.replace('-', '_')
@@ -219,9 +283,10 @@ class Optimisation_VAE(Plotting):
             os.makedirs(output_path)   
         return output_path
 
-    def predict_latents(self, *data, labels=None):
+    def predict_latents(self, *data, labels=None, val_set=False):
         self.data = data
         self.labels = labels
+        self.val_set = val_set
         generators =  self.generate_data(self.data, labels=self.labels)
         if self.batch_size is None:
             if self.labels is not None:
@@ -366,6 +431,14 @@ class Optimisation_AAE(Optimisation_VAE):
             if self.batch_size!=None:
                 x_same = []
                 x_cross = []
+                if self.joint_representation:
+                    for i in range(self.n_views):
+                        x_same.append(np.zeros((generators[i].dataset.shape[0], generators[i].dataset.shape[1])))
+                else:
+                    for i in range(self.n_views):
+                        x_same.append(np.zeros((generators[i].dataset.shape[0], generators[i].dataset.shape[1])))
+                        x_cross.append(np.zeros((generators[i].dataset.shape[0], generators[i].dataset.shape[1])))
+                        
                 for batch_idx, local_batch in enumerate(zip(*generators)):
                     local_batch = [local_batch_.to(self.device) for local_batch_ in local_batch]
                     z = self.encode(local_batch)
