868dbd5f373a7c7ce0edfcbbf2e6becffdd0f31f
alawryaguila <ana.lawryaguila@outlook.com>
2022-07-09 10:50:08 +0100

    delete vae classifier

--- tests/test_utilsdeep.py
+++ tests/test_utilsdeep.py
@@ -172,22 +172,3 @@ def test_AE():
         latent = models.predict_latents(test_1, test_2)
         recon = models.predict_reconstruction(test_1, test_2)
 
-def test_classiferVAE(): 
-    from multiae import VAE_classifier
-    train_1 = np.random.rand(200, 20)
-    train_2 = np.random.rand(200, 20)
-    test_1 = np.random.rand(50, 20)
-    test_2 = np.random.rand(50, 20)
-    train_labels = np.random.randint(2, size=200)
-    DEVICE = 'cpu'
-    parameters = {'hidden_layers': [[], [100,50,10]], 'batch_size': [None, 10]}   
-    param_combs = list(it.product(*(parameters[key] for key in parameters)))
-    path = str(os.getcwd()) + '/VAEclassifier'
-    for comb in param_combs:
-        params = {key: comb[idx] for idx, key in enumerate(parameters)}
-        models = VAE_classifier(input_dims=[20], n_labels=2, n_epochs=10, **params).to(DEVICE)
-        models.fit(train_1, train_2, labels=train_labels, output_path=path)
-        latent = models.predict_latents(test_1, test_2)
-        recon = models.predict_reconstruction(test_1, test_2)
-        pred = models.predict_labels(test_1, test_2)
-
