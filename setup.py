0e0f325a40703da938cd5f8958a9fa5ef4bdd4b4
alawryaguila <ana.lawryaguila@outlook.com>
2022-08-03 17:20:21 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 from setuptools import setup, find_packages
 import os
 
+
 def setup_package():
     data = dict(
-    name='multiae',
-    version='0.0.2',   
-    packages=find_packages(exclude=['*tests']),
-    author='Ana Lawry Aguila',
-    author_email='ana.lawryaguila@outlook.com',
-    url='https://github.com/alawryaguila/multiAE',
-    install_requires=[
-        'pytest',
-        'numpy>=1.17'
-        'scikit-learn~=0.24.1',
-        'scipy~=1.7.1',
-        'pandas~=1.2.3',
-        'matplotlib~=3.4.1',
-        'torch==1.10.2',
-        'torchvision~=0.11.3',
-        'pytorch-lightning>=1.5.5',
-        'umap-learn~=0.5.2',
-        'hydra-core~=1.2.0',
-    ],
-    description='A library for running multiview autoencoder models on medical imaging data'
+        name="multiae",
+        version="0.0.2",
+        packages=find_packages(exclude=["*tests"]),
+        author="Ana Lawry Aguila",
+        author_email="ana.lawryaguila@outlook.com",
+        url="https://github.com/alawryaguila/multiAE",
+        install_requires=[
+            "pytest",
+            "numpy>=1.22",
+            "scikit-learn~=0.24.1",
+            "scipy~=1.7.1",
+            "pandas",
+            "matplotlib~=3.4.1",
+            "torch>=1.10.2",
+            "torchvision~=0.11.3",
+            "pytorch-lightning>=1.6.0",
+            "umap-learn~=0.5.2",
+            "hydra-core~=1.2.0",
+        ],
+        description="A library for running multiview autoencoder models on medical imaging data",
     )
     setup(**data)
+
 
 if __name__ == "__main__":
     setup_package()
