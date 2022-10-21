e96efc2587da81cc3f0d70c43bc4d43c4dba48cd
alawryaguila <47194980+alawryaguila@users.noreply.github.com>
2022-10-21 13:26:30 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,17 @@
 # Configuration file for the Sphinx documentation builder.
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import warnings
 import datetime
+
+sys.path.insert(0, os.path.abspath("../.."))
 # -- Project information
 
 project = 'MultiAE'
