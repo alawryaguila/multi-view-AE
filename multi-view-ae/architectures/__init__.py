b3347c1243af6eddf5244fd460738ac6b025eb32
alawryaguila <47194980+alawryaguila@users.noreply.github.com>
2022-11-13 14:37:35 +0000

    Delete __init__.py

--- multi-view-ae/architectures/__init__.py
+++
@@ -1,7 +0,0 @@
-from .mlp import *
-from .cnn import *
-
-__all__ = ["Encoder", "VariationalEncoder", "Decoder", "VariationalDecoder", "Discriminator"]
-classes = __all__ 
-
-#TODO: use constants instead
